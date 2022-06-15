e5a4579dc027db4afe7009613a5cdd2806b441fe
Md.Suheb <suheb.cse@gmail.com>
2022-06-15 08:23:25 +0530

    all three pages validation completed

--- lib/app/modules/yourAddress/controllers/your_address_controller.dart
+++ lib/app/modules/yourAddress/controllers/your_address_controller.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../widgets/snackbar.dart';
+
 class YourAddressController extends GetxController {
+  final addressFormKey = GlobalKey<FormState>();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController landmarkController = TextEditingController();
+  TextEditingController pincodeController = TextEditingController();
+  TextEditingController cityController = TextEditingController();
   var intialSelectedState = "".obs;
 
   List<String> stateList = [
@@ -12,6 +19,48 @@ class YourAddressController extends GetxController {
     "Delhi",
     "Others"
   ];
+
+//* validation ------->>>>>
+
+  String? validAddress(adrs) {
+    if (adrs.toString().length < 3) {
+      return "Please enter your Address";
+    }
+  }
+
+  String? validLandmark(land) {
+    if (land.toString().length < 3) {
+      return "Please enter Nearby Location as Landmark";
+    }
+  }
+
+  String? validCity(city) {
+    if (city.toString().length < 3) {
+      return "Please enter your city";
+    }
+  }
+
+  String? validPinCode(zip) {
+    if (zip.toString().length < 6) {
+      return "Please enter 6 Digit zip code";
+    }
+  }
+
+  validateaddressDropDownFields() {
+    if (intialSelectedState.value.isEmpty) {
+      showRedSnackbar(
+        "Empty Selection Fields",
+        "Selection of your state is required",
+      );
+    }
+  }
+
+  checkAddress() {
+    if (addressFormKey.currentState!.validate() ||
+        validateaddressDropDownFields()) {
+      print("congratulation!!");
+    }
+  }
 
   @override
   void onInit() {
--- lib/app/modules/yourAddress/views/your_address_view.dart
+++ lib/app/modules/yourAddress/views/your_address_view.dart
@@ -14,59 +14,72 @@ class YourAddressView extends GetView<YourAddressController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(title: "Your Address"),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextformField(
-              hintText: "Adress",
-              prefixIcon: Icon(
-                Icons.location_city_rounded,
-                color: Color(0xff000075),
+      body: Form(
+        key: controller.addressFormKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextformField(
+                controller: controller.addressController,
+                validator: (adrs) => controller.validAddress(adrs),
+                hintText: "Adress",
+                prefixIcon: Icon(
+                  Icons.location_city_rounded,
+                  color: Color(0xff000075),
+                ),
               ),
-            ),
-            CustomTextformField(
-              hintText: "Landmark",
-              prefixIcon: Icon(
-                Icons.location_city_rounded,
-                color: Color(0xff000075),
+              CustomTextformField(
+                controller: controller.landmarkController,
+                validator: (land) => controller.validLandmark(land),
+                hintText: "Landmark",
+                prefixIcon: Icon(
+                  Icons.location_city_rounded,
+                  color: Color(0xff000075),
+                ),
               ),
-            ),
-            CustomTextformField(
-              hintText: "City",
-              prefixIcon: Icon(
-                Icons.location_city_rounded,
-                color: Color(0xff000075),
+              CustomTextformField(
+                controller: controller.cityController,
+                validator: (city) => controller.validCity(city),
+                hintText: "City",
+                prefixIcon: Icon(
+                  Icons.location_city_rounded,
+                  color: Color(0xff000075),
+                ),
               ),
-            ),
-            Obx(() => CustomDropdownTextFormField(
-                  hintText: "Select Your State",
-                  titleText: "State*",
-                  dropDownListOfItems: controller.stateList,
-                  onChanged: (selectedDomain) {
-                    controller.intialSelectedState.value = selectedDomain ?? "";
-                  },
-                  isEmpty: controller.intialSelectedState.value.isEmpty
-                      ? true
-                      : false,
-                  value: controller.intialSelectedState.value.isEmpty
-                      ? null
-                      : controller.intialSelectedState.value,
-                )),
-            CustomTextformField(
-              hintText: "Pin Code",
-              prefixIcon: Icon(
-                Icons.location_city_rounded,
-                color: Color(0xff000075),
+              Obx(() => CustomDropdownTextFormField(
+                    hintText: "Select Your State",
+                    titleText: "State*",
+                    dropDownListOfItems: controller.stateList,
+                    onChanged: (selectedDomain) {
+                      controller.intialSelectedState.value =
+                          selectedDomain ?? "";
+                    },
+                    isEmpty: controller.intialSelectedState.value.isEmpty
+                        ? true
+                        : false,
+                    value: controller.intialSelectedState.value.isEmpty
+                        ? null
+                        : controller.intialSelectedState.value,
+                  )),
+              CustomTextformField(
+                controller: controller.pincodeController,
+                validator: (zip) => controller.validPinCode(zip),
+                hintText: "Pin Code",
+                prefixIcon: Icon(
+                  Icons.location_city_rounded,
+                  color: Color(0xff000075),
+                ),
               ),
-            ),
-            CustomButton(
-                text: "Submit",
-                textColor: Colors.white,
-                border: Border.all(color: Color(0xff000075)),
-                backgroundColor: Color(0xff000075))
-          ],
+              CustomButton(
+                  navigation: (() => controller.checkAddress()),
+                  text: "Submit",
+                  textColor: Colors.white,
+                  border: Border.all(color: Color(0xff000075)),
+                  backgroundColor: Color(0xff000075))
+            ],
+          ),
         ),
       ),
     );
